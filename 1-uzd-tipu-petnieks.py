1424557f68ffa0b2de2af5c2e2811ffd023bd239
Betsija <43956352+Betsija@users.noreply.github.com>
2026-02-14 14:45:40 +0200

    Add files via upload

---
+++ 1-uzd-tipu-petnieks.py
@@ -0,0 +1,39 @@
+print(int("7") - int("5"))
+print(int("34") + int("8")- int("3"))
+x = 17
+y = 2
+print(x/y)
+print(x//y)
+print(x*y)
+print(x**y)
+print(x%y)
+x1 = 200
+y1 = 800
+print(x1%y1)
+k = 3
+l = [4, 6, 8]
+m = "Liene"
+print(type(k))
+print(type(l))
+print(type(m))
+a = 8
+b = 3
+print(type(a) == type(b))
+print("Abi ir integrāļi")
+c = 8
+d = 3.5
+print(type(c) == type(d))
+print("Otrs ir decimālis - float")
+f = 7
+g = 2
+print(f != y)
+print("Skaitļi nav vienādi")
+h = 2
+i = 3
+print(h >= i)
+print("Skaitlis h nav lielāks vai vienāds ar i")    
+
+
+
+
+
