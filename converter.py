df9532b01a06565f723492ba96d736e49abd9139
Betsija <43956352+Betsija@users.noreply.github.com>
2026-02-15 21:12:57 +0200

    Refactor distance and weight conversion in converter.py
    Mērķis: nostiprināt konstanšu lietošanu, aritmētiskos operatorus un f-string formatēšanu.

--- converter.py
+++ converter.py
@@ -2,9 +2,6 @@ KM_TO_MI = 0.62137
 km = float(input("Ievadi attālumu kilometros: "))
 mi = km * KM_TO_MI
 print("Attālums jūdzēs ir: {:.2f}".format(mi))
-
-
-
 KG_TO_LB = 2.20462
 kg = float(input("Ievadi svaru kilogramā: "))
 lb = kg * KG_TO_LB
