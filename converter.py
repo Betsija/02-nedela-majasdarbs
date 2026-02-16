c9093f959a9ce3d2eb5b7010e90a67c8fec556e6
Betsija <43956352+Betsija@users.noreply.github.com>
2026-02-16 09:23:27 +0200

    Update output messages to include units

--- converter.py
+++ converter.py
@@ -1,20 +1,19 @@
 KM_TO_MI = 0.62137
 km = float(input("Ievadi attālumu kilometros: "))
 mi = km * KM_TO_MI
-print("Attālums jūdzēs ir: {:.2f}".format(mi))
+print("Attālums jūdzēs ir: {:.2f} mi".format(mi))
 KG_TO_LB = 2.20462
 kg = float(input("Ievadi svaru kilogramā: "))
 lb = kg * KG_TO_LB
-print("Svars mārciņās ir: {:.2f}".format(lb))
+print("Svars mārciņās ir: {:.2f} lbs".format(lb))
 L_TO_GAL = 0.26417
 l = float(input("Ievadi tilpumu litros: "))
 gal = l * L_TO_GAL
-print("Tilpums galonos ir: {:.2f}".format(gal))
+print("Tilpums galonos ir: {:.2f} gal".format(gal))
 USD_TO_EUR = 0.8423502
 usd = float(input("Ievadi summu dolāros: "))
 eur = usd * USD_TO_EUR
-print("Summa eiro ir: {:.2f}".format(eur))
-
+print("Summa eiro ir: {:.2f} EUR".format(eur))
 
 
 
