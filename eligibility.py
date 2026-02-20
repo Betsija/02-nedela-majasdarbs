4c9f99e6b1726918c16269d8f75a50b1422f0af4
Liene Lukasevica <43956352+Betsija@users.noreply.github.com>
2026-02-20 12:02:22 +0200

    Implement eligibility check based on age
    Added a function to check age-related eligibility for voting, driving, car rental, student discounts, and senior discounts.

--- eligibility.py
+++ eligibility.py
@@ -1,1 +1,40 @@
+def check_eligibility():
+    try:
+        # Vecums
+        age = int(input("Ievadi savu vecumu: "))
 
+        if age < 0:
+            print("Ievadi vecumu virs 0.")
+            return
+
+        # Balsošana
+        if age >= 18:
+            print("- Drīkst balsot.")
+        else:
+            print("- Nedrīkst balsot.")
+            
+
+        # Autovadītāja apliecība
+        if age >= 18:
+            print("- Atļauts iegūt autovadītāja apliecību.")
+        else:
+            print("- Pārāk jauns, lai iegūtu autovadītāja apliecību.")
+        
+   # Auto īre
+        if age >= 21:
+            print("- Atļauts īrēt automašīnu.")
+        else:
+            print("- Pārāk jauns, lai īrētu automašīnu.")
+             # Studenta atlaide
+        if (16 <= age <= 26):
+            print("- Ir studenta atlaide.")
+                # Saņemt senioru atlaidi
+        if age >= 65:
+            print("- Var saņemt senioru atlaidi.")
+        
+
+    except ValueError:
+        print("Kļuda! Lūdzu ievadi vecumu ciparos!")
+
+# Run the function
+check_eligibility()
